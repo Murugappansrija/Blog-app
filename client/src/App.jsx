e1f2985025390839cd8362760b44e28753a7e143
murugappanDev <murugappandev@gmail.com>
2024-08-01 11:39:27 +0530

    create a header section for frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import DashBoard from "./Pages/DashBoard";
+import Projects from "./Pages/Projects";
+import About from "./Pages/About";
+import HeaderBar from './Component/HeaderBar';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div className="text-5xl">
-        eojpfpiheg
-       </div>
-    </>
+    <BrowserRouter>
+    <HeaderBar/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Register/>}/>
+      <Route path="/dashboard" element={<DashBoard/>}/>
+      <Route path="/project" element={<Projects/>}/>
+      <Route path="/about" element={<About/>}/>
+    </Routes>
+     
+    </BrowserRouter>
   );
 }
 
